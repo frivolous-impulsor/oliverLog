1d588747b844278b5a51e7ca7217fccf9ce3fae7
frivolous-impulsor <ultrapilot95@gmail.com>
2024-11-20 02:12:19 +0000

    extended title size

--- post/models.py
+++ post/models.py
@@ -4,13 +4,13 @@ from django.utils import timezone
 from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=256)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title} Post'
-    
+
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
